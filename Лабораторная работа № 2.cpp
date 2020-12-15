ab1dec7e870278935eb4be1f3757421634885c51
nKurganov <76050268+nKurganov@users.noreply.github.com>
2020-12-15 18:18:23 +0500

    Add files via upload

---
+++ Лабораторная работа № 2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    double n, m;
+    cout << " Введите значение n " << endl;
+    cin >> n;
+    cout << " Введите значение m " << endl;
+    cin >> m;
+    cout << "Значение выражения n+++m = " << n + ++m << endl;
+    cout << "Значение выражения n-- >m = " << (m-- > n) << endl;
+    cout << "Значение выражения n-- >m = " << (n-- > m) << endl;
+    system("pause");
+}
