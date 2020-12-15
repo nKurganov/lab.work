4a6f4b9dd2b58aa01621f6d272b59d52df068017
nKurganov <76050268+nKurganov@users.noreply.github.com>
2020-12-15 18:16:40 +0500

    Add files via upload

---
+++ Лабораторная работа № 1 задание 2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    float a, b, res;
+    cout << "Введите значение а" << endl;
+    cin >> a;
+    cout << "Введите значение b" << endl;
+    cin >> b;
+
+    res = (pow(a + b, 2) - (pow(a, 2) + 2 * a * b)) / (pow(b, 2));
+
+    cout << "Значение выражения = ";
+    cout << res << endl;
+    system("pause");
+}
