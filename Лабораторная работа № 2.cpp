8e11542b6b50f2a0582bfcd833ecb3c41821ef1f
nKurganov <76050268+nKurganov@users.noreply.github.com>
2021-02-07 10:30:15 +0500

    Add files via upload

---
+++ Лабораторная работа № 2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    int s = 0;
+    for (int n = 1; n < 200; n = n + 3)
+        s = n + s;
+    cout << "Сумма целых положительных чисел кратных трем и меньше 200 равна - " << s << endl;
+
+    system("pause");
+
+    return 0;
+}
